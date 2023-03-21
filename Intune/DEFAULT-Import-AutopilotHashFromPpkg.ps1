5c4f5a77eb62ee38bbd7f5daffc1d74cf95d5cd9
Thiago Beier <thiago.beier@gmail.com>
2023-03-21 17:16:32 -0400

    Updated instructions

--- Intune/DEFAULT-Import-AutopilotHashFromPpkg.ps1
+++ Intune/DEFAULT-Import-AutopilotHashFromPpkg.ps1
@@ -54,7 +54,7 @@ Start-Sleep -Seconds 5
 $AppId = 'YOUR-AZURE-AD-APP-ID'
 $TenantId = 'YOUR-TENANT-ID'
 $AppSecret = 'YOUR-AZURE-AD-APP-ID-SECRET'
-$GroupTag = "" #BULC code for Acquired company or BLANK for CSI Default => TL Joined
+$GroupTag = "" #BULC code for Acquired company or BLANK for Default => Hybrid Azure AD Joined
 
 "Installing Packages"
 
