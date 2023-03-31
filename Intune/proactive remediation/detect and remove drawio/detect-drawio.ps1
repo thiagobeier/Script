00bde5ad7dc82776ba085f3633e498b66b16dbec
Thiago Beier <thiago.beier@gmail.com>
2023-03-31 09:22:24 -0400

    post - drawio

---
+++ Intune/proactive remediation/detect and remove drawio/detect-drawio.ps1
@@ -0,0 +1,21 @@
+﻿#detect drawio
+
+try
+{
+$detecta = Get-AppxPackage | select * | where-object {$_ -like "*draw.io.draw.ioDiagrams*"}
+if ($detecta) {
+"removing $($detecta.Name)"
+#Remove-AppxPackage -AllUsers -Package $detecta.PackageFullName
+#echo "detected" | out-file detected.txt
+exit 1
+} else {
+    "do nothing"
+    #echo "not found" | out-file not-found.txt
+    exit 0
+    }
+ }
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
---
+++ Intune/proactive remediation/detect and remove drawio/remediate-drawio.ps1
@@ -0,0 +1,20 @@
+﻿#remove drawio
+
+try
+{
+$detecta = Get-AppxPackage | select * | where-object {$_ -like "*draw.io.draw.ioDiagrams*"}
+if ($detecta) {
+"removing $($detecta.Name)"
+ Remove-AppxPackage -AllUsers -Package $detecta.PackageFullName
+ echo "removed" | out-file removed.txt
+exit 0
+} else {
+    "do nothing"
+    exit 0
+    }
+ }
+catch{
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    exit 1
+}
