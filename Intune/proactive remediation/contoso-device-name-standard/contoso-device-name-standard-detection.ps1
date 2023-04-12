506fe3e64c08f472ae1006a5dad3defb1f4cbae2
Thiago Beier <thiago.beier@gmail.com>
2023-04-12 00:36:56 -0400

    Proactive Remediation Scripts
    Toast Notification Non-Standard Device Name

---
+++ Intune/proactive remediation/contoso-device-name-standard/contoso-device-name-standard-detection.ps1
@@ -0,0 +1,30 @@
+﻿
+# Detect Device Name and create BUXX BULC
+function Get-DsRegCmd {
+	$DsRegCmd = $env:COMPUTERNAME
+    return $Dsregcmd
+}
+
+$DSREG_Output = Get-DsRegCmd
+#$DSREG_Output 
+
+
+function Get-DeviceNameStatus {
+    #$DSREG_Output 
+    #clear
+    $name = $DSREG_Output
+
+    if ($name -like "DESKTOP*" -or $name -like "Auto*") { #{"y"} else {"n"}
+        #"default"
+        #buxx
+        return "Not compliant"
+         Write-Warning "Not Compliant"
+        Exit 1
+    } else {
+        return "Compliant"
+        Write-Output "Compliant"
+        Exit 0
+    }
+}
+
+Get-DeviceNameStatus
