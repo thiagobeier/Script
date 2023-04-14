aefb20c6e61c7a89d3251913e8a36d667a772fb0
Thiago Beier <thiago.beier@gmail.com>
2023-04-14 00:08:32 -0400

    new post
    new post

---
+++ Intune/ppkg/Import-AutopilotHash.ps1
@@ -0,0 +1,267 @@
+﻿<#
+Name: Import-AutopilotHash
+Author: Thiago Beier, twitter @thiagobeier, https://thebeier.com thiago.beier@gmail.com
+Version: 2.0
+.Synopsis
+   This script is meant to be used as part of provisioning package to automate importing autopilot hashes. It can be run from within the full OS or from the OOBE experience. Also send a notification to a Teams Channel
+   This script checks if Windows Autopilot Device exists and import / update it based on Default or Bsiness code
+.DESCRIPTION
+    Import-AutopilotHash is part of a solution to autopmatically upload autopilot hashes directly to Microsoft Intune without direct interaction.
+    It was created to be used as part of a provisioning package to allow hashes to be uploaded from the Out of Box experience with little to no interaction. After running the computer
+    will return to the out of box experience and the user can continue to log in or a technician can take it through pre-provisioning. Please note that this script will exit once the first stage
+    of the import has been completed. I recommend checking the Autopilot devices list to know when they upload has been completed.
+    This script will leverage MSGraph. It was built for authentication with a client secret, but could be adjusted to allow for certificate based authentication. Since the
+    client secret is hardcoded in the script, I recommend password protecting the PPKG file. The app registration used should also have limited permissions to limit it to being used for the
+    specific purpose.
+    Usage: Onboarding Windows devices to autopilot - Intune - Group Tag selected menu, where selection is from a central repository (TXT file from Azure blob storage)
+    Additonal info: Check PS2EXE project https://www.powershellgallery.com/packages/ps2exe/1.0.11 to wrap this PS1 into EXE
+.
+#>
+
+Clear-Host
+
+
+#region requirements
+$LogFolder = 'C:\Logs'
+#endregion
+
+#Date and time
+$dt = Get-Date -Format "dd-MM-yyyy-HH-mm-ss"
+$logfile = "$LogFolder\contoso-upload-hash-$dt.log"
+Start-Transcript -Path $logfile
+
+#region logfolder
+#Test to see if folder [$LogFolder]  exists"
+if (Test-Path -Path $LogFolder) {
+"Log folder exists!"
+} else {
+"Log folder doesn't exist."
+	New-Item -ItemType Directory -Path $LogFolder
+	}
+#endregion
+
+#region functions
+#Functions
+
+
+function CollectSerial {
+#add code here
+write-host -ForegroundColor Cyan "CollectSerial"
+}
+
+function UploadToList {
+#add code here
+write-host -ForegroundColor Cyan "UploadToList"
+}
+
+function TeamsNotification {
+#add code here
+write-host -ForegroundColor Cyan "TeamsNotification"
+}
+
+function UploadHashToIntune {
+#add code here
+write-host -ForegroundColor Cyan "UploadHashToIntune"
+
+#begin
+# Device
+# Gather Autopilot details
+$session = New-CimSession
+$serial = (Get-CimInstance -CimSession $session -Class Win32_BIOS).SerialNumber
+$devDetail = (Get-CimInstance -CimSession $session -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
+$hash = $devDetail.DeviceHardwareData
+#$env:COMPUTERNAME
+
+
+#powershell module
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -Confirm:$false
+#Install-Module Microsoft.Graph -Scope CurrentUser -Force -Confirm:$false
+#try this
+#Install-Module -Name Microsoft.Graph.Intune -Scope CurrentUser -Force -Confirm:$false
+#Install-Module -Name WindowsAutoPilotIntune -Scope CurrentUser -Force -Confirm:$false
+
+# Generate Access Token to use in the connection string to MSGraph
+# Contoso Tenant
+Import-Module Microsoft.Graph.Intune 
+Import-Module WindowsAutopilotIntune
+#this is the part that i had to hardcodeinto the runbook.
+$tenant = "letsintune.ca"
+$authority = "https://login.windows.net/$tenant"
+$clientid = '41427d2a-43de-4c12-9f3f-2004d9ff8da3'
+$tenantId = 'c24fd167-8ae3-438e-a7b5-a7dda448c5a9'
+$clientSecret = 'b1g8Q~N2SzJFzvR72rdgiQzCHdCJ2UPfb1vbeaz.'
+
+Update-MSGraphEnvironment -AppId $clientId -Quiet 
+Update-MSGraphEnvironment -AuthUrl $authority -Quiet 
+Connect-MSGraph -ClientSecret $ClientSecret -Quiet
+
+#Get-AutopilotDevice
+
+#$certificate = "CN=PowerShell App-Only"
+$certificate = "ADMTHEBEIERCert"
+Connect-MgGraph -ClientId $clientId -TenantId $tenantId -CertificateThumbprint "B4FE59A45A9968DC9C79B1DA022AB9E37AF8BB53"
+
+#device mgmt
+$alldevices = Get-AutopilotDevice | Select ID, serialnumber, Grouptag, addressableUserName
+$thisdevice = $alldevices | Where-Object {$_.serialNumber -eq "$serial"}
+$thisdevice
+
+if (!$thisdevice) {
+"not found in autopilot"
+#add device to autopilot
+Write-host "Adding $($TAG_Output) grouptag to $($env:COMPUTERNAME)"
+Add-AutoPilotImportedDevice -serialNumber $serial -hardwareIdentifier $hash -orderIdentifier $TAG_Output
+start-sleep -seconds 10
+
+while ($thisdevice.groupTag -ne $TAG_Output) {
+$alldevices = Get-AutopilotDevice | Select ID, serialnumber, Grouptag, addressableUserName
+$thisdevice = $alldevices | Where-Object {$_.serialNumber -eq "$serial"}
+$thisdevice.groupTag
+Write-Output "Waiting to import device to autopilot"
+}
+
+} else {
+if ($thisdevice.groupTag -eq $TAG_Output) {
+write-host "Device GroupTag matches - no update required"
+
+} else {
+#updates device tag to new tag
+#Add-AutoPilotImportedDevice -serialNumber $serial -hardwareIdentifier $hash -orderIdentifier $TAG_Output
+Get-AutopilotDevice -serial $serial | Set-AutopilotDevice -groupTag $TAG_Output
+while ($thisdevice.groupTag -ne $TAG_Output) {
+$alldevices = Get-AutopilotDevice | Select ID, serialnumber, Grouptag, addressableUserName
+$thisdevice = $alldevices | Where-Object {$_.serialNumber -eq "$serial"}
+$thisdevice.groupTag
+Write-Output "Waiting to update tag to $TAG_Output"
+start-sleep -seconds 10
+}
+}
+
+
+}
+#end
+
+}
+
+function CleanupAutopilotDevices {
+#cleanup autopilot devices
+#Start-Sleep -Seconds 60
+write-host "Autopilot imported device cleanup"
+Get-AutopilotImportedDevice | Where-Object { $_.serialNumber -match "$serial" }
+(Get-AutopilotImportedDevice | Where-Object { $_.serialNumber -match "$serial" }).count
+$deviceStatuses = Get-AutoPilotImportedDevice
+$deviceStatuses | Remove-AutopilotImportedDevice
+}
+
+
+function ThisSelection {
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# Download file from Azure blob storage
+#$locatoinidfile = "https://csi0101.blob.core.windows.net/scripts/locationid.txt"
+$locatoinidfile = "https://csi0101.blob.core.windows.net/scripts/newlocationid.txt"
+Invoke-WebRequest -Uri $locatoinidfile -OutFile locationidv2.txt
+
+# Define the path to your text file
+$file = "locationidv2.txt"
+
+# Read the contents of the text file into an array
+$options = Get-Content $file
+
+# Create a new Windows Forms form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Select a (GroupTag)"
+$form.AutoSize = $true # enable automatic sizing
+$form.StartPosition = "CenterScreen" # center the form on the screen
+
+
+# Create a new Windows Forms combo box (drop-down menu)
+$combo = New-Object System.Windows.Forms.ComboBox
+$combo.Location = New-Object System.Drawing.Point(10, 10)
+$combo.Width = 150
+# Center the combo box within the form
+$combo.Left = ($form.ClientSize.Width / 2) - ($combo.Width / 2)
+$combo.Top = 20
+
+# Add each option from the text file to the combo box
+foreach ($option in $options) {
+[void] $combo.Items.Add($option)
+}
+
+# Add the combo box to the form
+$form.Controls.Add($combo)
+
+# Create a new Windows Forms button
+$button = New-Object System.Windows.Forms.Button
+$button.Text = "OK"
+$button.Location = New-Object System.Drawing.Point(220, 10)
+$button.Width = 70
+#$button.Height = 70
+# Center the combo box within the form
+$button.Left = ($form.ClientSize.Width / 2) - ($button.Width / 2)
+$button.Top = 70
+
+
+# Add an event handler to the button's Click event
+$button.Add_Click({
+
+$global:selectedOption = $combo.SelectedItem
+$form.Close()
+# Show confirmation dialog
+[System.Windows.Forms.MessageBox]::Show("Are you sure you want to select '$selectedOption'?", "Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question) | Out-Null
+
+
+})
+
+# Add the button to the form
+$form.Controls.Add($button)
+
+# Show the form as a dialog box
+[void] $form.ShowDialog()
+
+
+# Display the selected option
+if ($global:selectedOption) {
+Write-Host -ForegroundColor Green "Selected GroupTag: $selectedOption"
+
+return $selectedOption
+
+} else {
+Write-Host -ForegroundColor Red "No option selected."
+}
+
+} #end of ThisSelection function
+
+
+$TAG_Output = ThisSelection
+$groupTag = $TAG_Output
+$groupTag
+
+# Function to upload imported / updated devices to SharePoint List
+# Detect Device Name
+function Get-DsRegCmd {
+# Dsregcmd powershell
+$DsRegCmd = $env:COMPUTERNAME
+return $Dsregcmd
+}
+
+$DSREG_Output = Get-DsRegCmd
+#$DSREG_Output
+
+UploadHashToIntune
+
+CleanupAutopilotDevices
+
+# Get Serial & update to Intune
+CollectSerial
+
+UploadHashToIntune
+
+# UploadItem to Spt online lits
+UploadToList
+
+# Send Teams notification
+TeamsNotification
+
+Stop-Transcript
---
+++ Intune/ppkg/create-vm-hyperv.ps1
@@ -0,0 +1,158 @@
+﻿<#
+.SYNOPSIS
+This script creates a vm with specified Name at c:\VMS\ attaching existing ISO from c:\VMS\isos\
+This powershell will create c:\VMS\  and  c:\VMS\isos\ if don't exist
+
+.DESCRIPTION
+This is customized for my Hyper-v running on Microsoft Windows 11 Enterprise
+Have your ISO file downloaded on the folder before you run this powershell
+Update $vmFolderPath $isosFolderPath $myvmname and $isofile prior you run this script
+
+.HARDWARE
+System Manufacturer	Dell Inc.
+System Model	    Latitude 5520
+Installed Physical Memory (RAM)	32.0 GB
+
+.REFERENCES
+https://docs.microsoft.com/en-us/powershell/module/hyper-v/set-vmfirmware?view=windowsserver2019-ps
+https://www.itexperience.net/fix-this-device-cant-use-a-trusted-platform-module/
+
+
+.NOTES
+Author: Thiago Beier
+Email: thiago.beier@gmail.com
+Blog: https://thebeier.com
+LinkedIn: https://www.linkedin.com/in/tbeier/
+Twitter: https://twitter.com/thiagobeier
+Date: 03/29/2023
+Version: 1.0
+#>
+
+# Variables
+#create folder if don't exist
+$vmFolderPath = "c:\vms"
+$isosFolderPath = "c:\vms\isos"
+
+#define vm name
+$myvmname = "NULL"
+#$isofile = "c:\vms\isos\en-us_windows_11_business_editions_version_22h2_updated_oct_2022_x64_dvd_080047de.iso" #windows 11 ISO
+$isofile = "c:\vms\isos\en-us_windows_server_2022_updated_dec_2022_x64_dvd_14fe3ddc.iso" #SERVER 2022
+
+#create $vmFolderPath and $isosFolderPath if don't exist
+
+if (!(Test-Path $vmFolderPath)) {
+    New-Item -ItemType Directory -Path $vmFolderPath | Out-Null
+}
+
+if (!(Test-Path $isosFolderPath)) {
+    New-Item -ItemType Directory -Path $isosFolderPath | Out-Null
+}
+
+# Functions
+#This is a yesNo prompt funcion used on CreateVM function 
+
+function Get-YesNoPrompt {
+    [CmdletBinding()]
+    param (
+        [string] $Message = "Are you sure you want to proceed?"
+    )
+
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Proceed with the operation."
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Cancel the operation."
+    $choices = [System.Management.Automation.Host.ChoiceDescription[]] ($yes, $no)
+    $defaultChoice = [System.Management.Automation.Host.ChoiceDescription] $no
+
+    $decision = $Host.UI.PromptForChoice($null, $Message, $choices, 1)
+
+    if ($decision -eq 0) {
+        return $true
+    }
+    else {
+        return $false
+    }
+}
+
+
+
+#This function creates the VM from variable using ISO from variables under $vmFolderPath = "c:\vms" and $isosFolderPath = "c:\vms\isos"
+#Asks to confirm before proceeding
+
+function CreateVM {
+
+#prompt yesNo function
+if (Get-YesNoPrompt "Are you sure you want to proceed with VM creation => VMName: $($myvmname) and $($isofile) ?") {
+    # User chose "Yes", so proceed with the operation.
+
+
+if ((Test-Path $isofile) -and !(get-vm -Name $myvmname -ErrorAction SilentlyContinue)) { #checks for $myvmname and $isofile ISO not found = error, VM same name exists ERROR
+write-host -ForegroundColor Green "Creating VM: $($myvmname) from ISO: $($isofile)" 
+#create vm on hyperv
+New-VM -Name $myvmname -MemoryStartupBytes 8GB -BootDevice VHD -NewVHDPath C:\VMS\$myvmname\$myvmname.vhdx -Path C:\VMS\$myvmname\ -NewVHDSizeBytes 60GB -Generation 2 -Switch "Default Switch"
+Add-VMDvdDrive -Path $isofile -VMName $myvmname
+Set-VMProcessor -VMName "$myvmname" -count 2
+
+#update, change boot order
+$myvm = Get-VMFirmware -VMName $myvmname
+$myvm.bootorder #default vm has pxe, hdd, dvddrive order
+$hddrive = $myvm.BootOrder[0]
+$pxe = $myvm.BootOrder[1]
+$dvddrive = $myvm.BootOrder[2]
+
+Set-VMFirmware -VMName $myvmname -BootOrder $dvddrive,$hddrive,$pxe #update/change bootorder to dvd, hdd, pxe
+$myvm = Get-VMFirmware -VMName $myvmname
+$myvm.bootorder
+
+#Enable TPM module on vm
+
+#Type the following cmdlet to import the Hyper-V module:
+import-module Hyper-V
+#Copy-paste the following cmdlets to configure a valid key protector
+$owner = Get-HgsGuardian UntrustedGuardian
+$kp = New-HgsKeyProtector -Owner $owner -AllowUntrustedRoot
+#You have now created a valid key protector. You may now apply the HgsKeyProtector to the VM:
+Set-VMKeyProtector -VMName $myvmname -KeyProtector $kp.RawData
+#(obviously, replace <VMname> with the name of your Virtual Machine, as shown in HyperV Manager.
+#You can now turn TPM with this cmdlet:
+Enable-VMTPM -VMName $myvmname
+
+#start vm and connect
+Checkpoint-VM -Name $myvmname -SnapshotName "FreshVM" #before setup / hash uploaded
+
+#sleep for 10 seconds
+Start-Sleep -Seconds 10
+
+#start vm and connect to it
+Start-VM -VMName $myvmname
+vmconnect.exe localhost $myvmname
+
+} else {write-host -ForegroundColor Red "ISO file not found or VM exists"}
+
+}
+else {
+    # User chose "No", so cancel the operation.
+}
+
+}
+
+
+<#
+Additional commands
+
+#save a checkpoint
+#Checkpoint-VM -Name $myvmname -SnapshotName "OSready" #before setup / hash imported to autopilot if that's the case
+#Checkpoint-VM -Name $myvmname -SnapshotName "Finished Windows install" #after hash uploaded
+
+#dvd
+#Get-VM $myvmname | Get-VMDvdDrive
+#Set-VMDvdDrive -VMName $myvmname -ControllerNumber 0 -ControllerLocation 1 -Path $null
+#Get-VMDvdDrive -VMName $myvmname | Set-VMDvdDrive -Path "C:\vms\ISOS\Tools.iso"
+#Get-VMDvdDrive -VMName $myvmname | Set-VMDvdDrive -Path "C:\vms\ISOS\USB.iso"
+
+#Get-Command -Module hyper-v | Out-GridView
+#get-vm -Name $myvmname
+
+#>
+
+
+
+CreateVM
